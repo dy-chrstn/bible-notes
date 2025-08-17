e022a2bf36074aa81bb8af6db4455154de4e01e3
Andrei <dy.cavd@gmail.com>
2025-08-18 07:13:27 +0800

    feat: add note list screen

--- lib/features/notes/screens/note_list.screen.dart
+++ lib/features/notes/screens/note_list.screen.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class NoteListScreen extends StatelessWidget {
   const NoteListScreen({super.key});
@@ -7,10 +8,33 @@ class NoteListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Text('Note List Screen'),
-      ],
+    return MasonryGridView.count(
+      padding: const EdgeInsets.all(16),
+      crossAxisCount: 2,
+      mainAxisSpacing: 8,
+      crossAxisSpacing: 8,
+      itemCount: 20,
+      itemBuilder: (context, index) => Card.outlined(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Title ${index + 1}',
+                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              ...List.generate(
+                index + 1,
+                (i) => Text(
+                  'Text ${i + 1}',
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
